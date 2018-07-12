336de414d326b60ba0086ab19c7241e4f8887748
MultiEx Network <41112179+MultiEx@users.noreply.github.com>
2018-07-12 17:58:55 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -202,7 +202,7 @@ const static boost::uuids::uuid CRYPTONOTE_NETWORK =
 };
 
 const char* const SEED_NODES[] = {
-  "64.52.84.29:17770", //rock
-  "145.239.88.119:11999", //cision
+  "64.52.84.29:17770", //viralteam
+  "64.190.90.251:17770", //tanja
 };
 } // CryptoNote
