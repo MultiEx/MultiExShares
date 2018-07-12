21159790d404999510f52e55696f39ed63b9da24
MultiEx Network <41112179+MultiEx@users.noreply.github.com>
2018-07-12 20:22:59 +0200

    Update WalletConfig.h

--- src/zedwallet/WalletConfig.h
+++ src/zedwallet/WalletConfig.h
@@ -35,7 +35,7 @@ namespace WalletConfig
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
 
     /* Where can your users contact you for support? E.g. discord */
-    const std::string contactLink = "http://multiex.io";
+    const std::string contactLink = "https://multiex.io";
 
 
     /* The number of decimals your coin has */
